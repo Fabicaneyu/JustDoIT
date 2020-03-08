6140f29a43db2218c0b4843b4b2eaf189fc95272
Messias <messias.d.carvalho@hotmail.com>
2020-03-08 18:24:26 -0300

    cadastro/login

--- AplicacaoSpringBoot/database-connection/src/main/java/com/connection/databaseconnection/usuario/UserController.java
+++ AplicacaoSpringBoot/database-connection/src/main/java/com/connection/databaseconnection/usuario/UserController.java
@@ -1,19 +1,28 @@
 package com.connection.databaseconnection.usuario;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Service
 @Controller
 public class UserController {
-
-    @RequestMapping("/")
+    @Autowired
+    private UsuarioRepository repository;
+    @RequestMapping(value="login", method = RequestMethod.GET)
     public String index(){
         return "login";
     }
+    @RequestMapping(value="login", method = RequestMethod.POST)
+    public String index(Usuario usuario){
+        repository.save(usuario);
+        return "redirect:login";
+    }
 
-    private UsuarioRepository repository;
+
+
 
     public UserController(UsuarioRepository repository) {
         super();
